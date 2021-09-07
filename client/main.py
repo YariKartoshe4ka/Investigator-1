64b4e025ed745872345f3f9fa75615cd8c897d77
YariKartoshe4ka <yaroslav.kikel.06@inbox.ru>
2021-09-07 06:24:25 -0400

    Изменение принципа отправки команд в iClient

--- client/main.py
+++ client/main.py
@@ -7,9 +7,11 @@ from kivy.network.urlrequest import UrlRequest
 from kivy.utils import platform
 from urllib.parse import quote
 from socket import gethostbyname
-from stream import StreamViewer
 import os
 import plyer
+
+from stream import StreamViewer
+
 
 if platform == 'android':
     from android.permissions import request_permissions, Permission
@@ -30,28 +32,75 @@ class MainScreen(Screen):
         online.icon = 'icons/online_true.png'
         online.size = (30, 30)
 
-    def send(self, command):
+    def send(self, command: bytes):
         root = App.get_running_app().root
 
         timeout = root.config.getdefaultint('network', 'timeout', root.config_defaults['network']['timeout'])
         host = root.config.getdefault('network', 'host', root.config_defaults['network']['host'])
-        command = quote(command)
+        command = quote(b'\x01' + command + b'\xff')
 
         url = f'http://{host}:80/api?cmd={command}'
 
-        UrlRequest(url, on_success=self.set_online_true,
-                        on_failure=self.set_online_false,
-                        on_error=self.set_online_false,
-                        on_redirect=self.set_online_false,
-                        timeout=timeout / 1000)
+        r = UrlRequest(url, on_success=self.set_online_true,
+                            on_failure=self.set_online_false,
+                            on_error=self.set_online_false,
+                            on_redirect=self.set_online_false,
+                            timeout=timeout / 1000)
+
+    def send_speed(self, value):
+        command = b''
+
+        closer = lambda x, y, z: x if abs(x - y) <= abs(z - y) else z
+        start = closer(closer(0, value, 127), value, 255)
+
+        if start == 0:
+            command += b'\x0a'
+        elif start == 127:
+            command += b'\x0b'
+        else:
+            command += b'\x0c'
+
+        length = start - value
+
+        if length > 0:
+            for i in range(abs(length)):
+                command += b'\x0e'
+        elif length < 0:
+            for i in range(abs(length)):
+                command += b'\x0d'
+
+        self.send(command)
+
+    def send_angle(self, value):
+        command = b''
+
+        closer = lambda x, y, z: x if abs(x - y) <= abs(z - y) else z
+        start = closer(closer(0, value, 67), value, 135)
+
+        if start == 0:
+            command += b'\x0f'
+        elif start == 67:
+            command += b'\x10'
+        else:
+            command += b'\x11'
+
+        length = start - value
+
+        if length > 0:
+            for i in range(abs(length)):
+                command += b'\x13'
+        elif length < 0:
+            for i in range(abs(length)):
+                command += b'\x12'
+
+        self.send(command)
 
     def change_stream_visibility(self):
         if self.ids['stream_viewer'].is_alive:
             self.ids['stream_viewer'].stop()
         else:
             self.ids['stream_viewer'].start()
-        self.send('ping')
-
+        self.send(b'')
 
 
 class SettingsScreen(Screen):
@@ -79,7 +128,8 @@ class InvestigatorClientApp(App):
     save_path = os.path.join(plyer.storagepath.get_downloads_dir(), 'iClient')
 
     config_defaults = {'network': {'host': host,
-                                   'timeout': 3000},
+                                   'timeout': 3000,
+                                   'logging': False},
                        'stream': {'fps': 30,
                                   'save_path': save_path}}
 
