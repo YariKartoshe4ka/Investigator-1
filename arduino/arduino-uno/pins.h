abb0107b54a0ccd90f2b9cd7a8a54c39ccf0735f
YariKartoshe4ka <yaroslav.kikel.06@inbox.ru>
2021-09-08 08:32:50 -0400

    Исправление неверного определения пинов

--- arduino/arduino-uno/pins.h
+++ arduino/arduino-uno/pins.h
@@ -5,9 +5,9 @@
 #define IN2_B_PIN            3
 #define IN1_B_PIN            4
 #define EN_B_PIN             5
-#define IN2_A_PIN            6
-#define IN1_A_PIN            7
-#define EN_A_PIN             8
+#define EN_A_PIN             6
+#define IN2_A_PIN            7
+#define IN1_A_PIN            8
 #define SERIAL_RX_PIN        10
 #define SERIAL_TX_PIN        11
 
